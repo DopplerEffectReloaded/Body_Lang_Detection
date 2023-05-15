8a0cebaa3a31400ca4cfcbbf55a0e6ef40a0ea37
= <adityadharap3@gmail.com>
2023-05-15 20:28:22 +0530

    Added detect function

---
+++ motion_detector/classifying_model/preprocessing.py
@@ -0,0 +1,47 @@
+# This file Processes the images, detects the poses using movenet API of tensorflow.js,
+# and creates CSV files with its coordinates
+
+# All dependencies required:
+import tensorflow as tf
+from movenet import Movenet
+from data import BodyPart
+import os
+import wget
+import tqdm
+import pandas as pd
+import numpy as np
+import csv
+
+# This installs the required file in the current directory
+if "movenet_thunder.tflite" not in os.listdir():
+    wget.download("https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/float16/4?lite-format=tflite",
+                  "movenet_thunder.tflite")  # Downloads the thunder (more precise) model of movenet
+
+movenet = Movenet("movenet_thunder")
+
+
+def detect(input_tensor, inference_count = 2):
+    """
+    This function runs the detection algorithm on the input image.
+
+    Params:
+    input_tensor: This is a input of dimensions [height, width, 3] (3 is the image channel) tensor of type tf.float32
+                The height and width can be any given height and width as tensor resizes it according to need.
+    inference_count: This is the number of times the model should run on the same image to improve accuracy.
+                Default value : 2, Recommended Maximum : 5
+
+    Returns:
+        A person entity as detected by Movenet.SinglePose
+    """
+    im_height, im_width, channel = input_tensor.shape
+
+    # Detecting poses input image
+
+    movenet.detect(input_tensor.numpy(), reset_crop_region=False) # Set to false since trained on static images
+
+    # Uses prev result to find ROI and increase accuracy
+
+    for _ in range(inference_count):
+        detect_person = movenet.detect(input_tensor.numpy(), reset_crop_region=False)
+
+    return detect_person
