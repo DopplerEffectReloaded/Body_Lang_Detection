2ce746398c7ed4dd3af8b2de30ed5737941d8239
= <adityadharap3@gmail.com>
2023-05-16 12:21:42 +0530

    Finished Process Functionality and added helper method

--- motion_detector/classifying_model/preprocessing.py
+++ motion_detector/classifying_model/preprocessing.py
@@ -50,12 +50,70 @@ def detect(input_tensor, inference_count=2):
 class PreProcessor:
 
     def __init__(self, images_in_dir, csv_out_path):
-        self._images_in_dir = images_in_dir
-        self._csv_out_path = csv_out_path
-        self._csv_out_folder_per_pose = "csv_for_each_pose"
-        self._message = []
+        self.images_in_dir = images_in_dir
+        self.csv_out_path = csv_out_path
+        self.csv_out_folder_per_pose = "csv_for_each_pose"
+        self.message = []
 
-        if self._csv_out_folder_per_pose not in os.listdir():
-            os.makedirs(self._csv_out_folder_per_pose)
+        if self.csv_out_folder_per_pose not in os.listdir():
+            os.makedirs(self.csv_out_folder_per_pose)
 
-        self._pose_names = sorted([i for i in os.listdir(images_in_dir)])
+        self.pose_names = sorted([i for i in os.listdir(images_in_dir)])
+
+    def process(self, min_detect_threshold=0.1):
+        """
+        Processes the images in the folder path specified.
+
+        Params:
+        min_detect_threshold: The minimum score the image should pass to be transferred to the csv file
+        Void method
+        """
+
+        for pose in self.pose_names:
+            # Processes the images folder by folder
+            pose_folder = os.path.join(self.images_in_dir, pose)
+            output_path = os.path.join(self.csv_out_folder_per_pose, pose + ".csv")
+
+            with open(output_path, 'w') as output_file:
+                out_write = csv.writer(output_file, quoting=csv.QUOTE_MINIMAL)
+
+                image_names = sorted([i for i in os.listdir(pose_folder)])
+                count_valid = 0
+                for image in tqdm.tqdm(iterable=image_names, desc="\n-------PROCESSING FILES-----------\n",
+                                       total=len(image_names)):
+                    path = os.path.join(pose_folder, image)
+
+                    try:
+                        image = tf.io.read_file(path)
+                        image = tf.io.decode_jpeg(image)
+                    except:
+                        self.message.append("Skipped" + path + "Invalid Image")
+                        continue
+
+                    if image.shape[2] != 3:
+                        self.message.append("Skipped" + path + "Image is not RGB")
+                        continue
+
+                    person = detect(image)
+
+                    min_score = min([keypoint.score for keypoint in person.keypoints])
+                    image_pass = min_score > min_detect_threshold
+                    if not image_pass:
+                        self.message.append("Skipped" + path + "Pose not detected accurately")
+                        continue
+                    count_valid += 1
+                    # Save coordinates if the keypoints of all poses are above threshold
+
+                    pose_keypoints = np.array([[keypoint.coordinate.x, keypoint.coordinate.y, keypoint.score]
+                                               for keypoint in person.keypoints], dtype=np.float32)
+
+                    coordinates = pose_keypoints.flatten().astype(np.str).tolist()
+                    out_write.writerow([image] + coordinates)
+
+        print(self.message)
+
+        all_coordinates_df = self.all_coordinates_as_dataframe()
+        all_coordinates_df.to_csv(self.csv_out_path, index=False)
+
+    def all_coordinates_as_dataframe(self):
+        pass
