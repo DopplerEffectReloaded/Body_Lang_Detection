8352f12ec7cd2e1179f75ca3262c2f784ba04f79
= <adityadharap3@gmail.com>
2023-05-18 17:40:29 +0530

    BugfixV2

--- vidFrame_to_img_converter.py
+++ vidFrame_to_img_converter.py
@@ -5,13 +5,13 @@ import numpy as np
 def vid_convert(filename, count_in):
     cap = cv.VideoCapture(filename)
     if not cap.isOpened():
-        exit(1)
+        print("fail")
     count = count_in
     while True:
         retval, frame = cap.read()
         if frame is None:
             break
-        cv.imwrite("C:\\Users\\SACHIN\\Body_Lang_Detection\\motion_detector\\classifying_model\\poses\\train\\frame%d.jpg" % count, frame)
+        cv.imwrite("C:\\Users\\SACHIN\\Body_Lang_Detection\\motion_detector\\classifying_model\\poses\\train\\shoulder\\frame%d.jpg" % count, frame)
         count += 1
     return count
 
@@ -19,4 +19,4 @@ def vid_convert(filename, count_in):
 if __name__ == '__main__':
     count = 0
     for i in range(6):
-        count = vid_convert("C:\\Users\\SACHIN\\hand_on_hip\\" + str(i) + ".mp4", count)
+        count = vid_convert("C:\\Users\\SACHIN\\hand_on_shoulder\\" + str(i+1) + ".mp4", count)
