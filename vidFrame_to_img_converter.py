2a82d94a27cb746294604c24187d2b912ff8ef31
= <adityadharap3@gmail.com>
2023-05-18 17:05:19 +0530

    Bugfix in count

--- vidFrame_to_img_converter.py
+++ vidFrame_to_img_converter.py
@@ -1,20 +1,24 @@
 import cv2 as cv
 
 
-def vid_convert(filename):
-
+def vid_convert(filename, count_in):
     cap = cv.VideoCapture(filename)
-    count = 0
-    # if not cap.isOpened():
-    #     exit(0)
+    if not cap.isOpened():
+        exit(0)
+    count = count_in
     while True:
-
         retval, frame = cap.read()
         if frame is None:
             break
-        cv.imwrite("C:\\Users\\SACHIN\\motion_training\\frames\\frame%d.jpg" % count, frame)
+        cv.imwrite("D:\\CBT\\bagZipping\\frames\\frame%d.jpg" % count, frame)
         count += 1
+    return count
 
+
+if __name__ == '__main__':
+    count = 0
+    for i in range(23):
+        count = vid_convert("D:\\CBT\\bagZipping\\" + str(i) + ".mp4", count)
 
 if __name__ == '__main__':
     for i in range(23):
